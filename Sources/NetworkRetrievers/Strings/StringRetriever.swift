8909ad70bc7b7eae68901c1e0c19487d0f8111c9
Joseph Wardell <jay@jaywardell.me>
2025-02-01 07:03:49 -0500

    StringRetriever.retrieve() falls back to using a FileURLRetriever if the URL is not an http URL. When it loads data from a file URL, it goes through all known encodings in roughly preponderance order until it finds one that can produce a String.

--- Sources/NetworkRetrievers/Strings/StringRetriever.swift
+++ Sources/NetworkRetrievers/Strings/StringRetriever.swift
@@ -25,10 +25,58 @@ public enum StringRetriever: Sendable {
         headers: [String: String]? = nil,
         configuration: URLSessionConfiguration = .default
     ) async throws -> String {
-        let (data, response) = try await HTTPRetriever.retrieve(url, headers: headers, configuration: configuration)
-        guard let string = String(data: data, textEncodingName: response.textEncodingName, fallback: String.Encoding.utf8) else {
+        do {
+            let (data, response) = try await HTTPRetriever.retrieve(url, headers: headers, configuration: configuration)
+            guard let string = String(data: data, textEncodingName: response.textEncodingName, fallback: String.Encoding.utf8) else {
+                throw Error.Decoding(url: url)
+            }
+            return string
+        }
+        catch HTTPRetriever.Error.NotAnHTTPURL {
+
+            let data = try await FileURLRetriever.retrieve(url)
+            
+            for encoding in String.Encoding.prioritziedEncodings {
+                guard let decoded = String(data: data, encoding: encoding) else { continue }
+                return decoded
+            }
+            
+            // no known encoding worked to create a String from the data
+            // so for our purposes we will throw an error
             throw Error.Decoding(url: url)
         }
-        return string
     }
+}
+
+fileprivate extension String.Encoding {
+    
+    /// the oncodings made available by Foundation as of macOS 10.10, iOS 8
+    /// in roughly the order of their likelihood to be encountered in the wild
+    /// starting with more standard formats
+    /// and moving to more platfor-specific ones
+    static var prioritziedEncodings: [String.Encoding] { [
+        .utf8,
+        .ascii,
+        .unicode,
+        .utf16,
+        .utf32,
+        .isoLatin1,
+        .isoLatin2,
+        .utf16BigEndian,
+        .utf16LittleEndian,
+        .utf32BigEndian,
+        .utf32LittleEndian,
+        .symbol,
+        .nonLossyASCII,
+        .japaneseEUC,
+        .shiftJIS,
+        .windowsCP1251,
+        .windowsCP1252,
+        .windowsCP1253,
+        .windowsCP1254,
+        .windowsCP1250,
+        .iso2022JP,
+        .macOSRoman,
+        .nextstep,
+    ] }
 }
